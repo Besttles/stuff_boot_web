c27f987201607ee4c13b9e33c61f49ae38356fd9
Besttles <bwh15543272388@163.com>
2018-08-07 17:54:27 +0800

    添加注释

--- src/main/java/com/stuff/realm/FisterCon.java
+++ src/main/java/com/stuff/realm/FisterCon.java
@@ -1,9 +1,10 @@
-package com.example.demo.realm;
+package com.stuff.realm;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value="/boot")
 public class FisterCon {
 @RequestMapping(value ="/request")
 public String getPro(){
